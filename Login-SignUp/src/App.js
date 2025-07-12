9e784d3ab125328b3a30cb799f25eb2db17ff90f
Rimsha Fatima <143187208+FatimaRimsha@users.noreply.github.com>
2025-07-12 16:56:36 +0530

    Add files via upload

---
+++ Login-SignUp/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSignUp from './components/LoginSignUp/LoginSignUp';
+function App() {
+  return (
+    <div>
+      <LoginSignUp/>
+    </div>
+  );
+}
+
+export default App;
